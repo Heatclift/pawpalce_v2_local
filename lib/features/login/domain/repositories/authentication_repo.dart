aecfe99bedcad102b3857e1481245a61f07e86d0
Heatclift <hcalmerin@gmail.com>
2024-04-05 22:14:26 +0800

    add firebase installation

--- lib/features/login/domain/repositories/authentication_repo.dart
+++ lib/features/login/domain/repositories/authentication_repo.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_app_installations/firebase_app_installations.dart';
 import 'package:pawplaces/common/data/models/user_session.dart';
 import 'package:pawplaces/common/data/sources/pawplace_api.dart';
 
@@ -6,9 +7,9 @@ class AuthenticationRepository {
 
 ///// returns session info token
   Future<String?> verifyNumber(int phoneNumer) async {
-    // String? deviceId = await PlatformDeviceId.getDeviceId;
+    String? installId = await FirebaseInstallations.instance.getId();
 
-    var res = await api.verifynumber(phoneNumer, 'deviceId');
+    var res = await api.verifynumber(phoneNumer, installId);
     if (res.isSuccessful) {
       var data = res.body["data"];
       if (data != null) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import app_links
 import file_selector_macos
 import firebase_analytics
 import firebase_app_check
+import firebase_app_installations
 import firebase_auth
 import firebase_core
 import firebase_crashlytics
@@ -27,6 +28,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FirebaseInstallationsPlugin.register(with: registry.registrar(forPlugin: "FirebaseInstallationsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
